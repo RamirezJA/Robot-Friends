50bf190f3a7bbc193622b489fc68cddf0ea325a7
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-05-01 11:36:03 -0700

    scroll

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { render } from "react-dom";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
@@ -33,7 +34,9 @@ class App extends Component {
       <div className="tc">
         <h1 className="f2">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
