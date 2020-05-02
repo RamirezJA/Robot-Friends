b1cf3fd07be7a6dc60cc2124aaa93ab1aac6f10b
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-05-01 20:48:58 -0700

    Updates

---
+++ precache-manifest.3e9f52daa743b01c7dfe29a5c57b0e57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffa6988749559b47468028c2064f6e6",
+    "url": "//Robot-Friends/index.html"
+  },
+  {
+    "revision": "62de9c2f950bce405543",
+    "url": "//Robot-Friends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "973a18ad7d2da8fb6963",
+    "url": "//Robot-Friends/static/css/main.434b6f71.chunk.css"
+  },
+  {
+    "revision": "62de9c2f950bce405543",
+    "url": "//Robot-Friends/static/js/2.9d798cdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//Robot-Friends/static/js/2.9d798cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973a18ad7d2da8fb6963",
+    "url": "//Robot-Friends/static/js/main.772fadf8.chunk.js"
+  },
+  {
+    "revision": "322620a34f5882dbb102",
+    "url": "//Robot-Friends/static/js/runtime-main.d8c8a841.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "//Robot-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
